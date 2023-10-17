8f43778c34da5dd5dae3121829bf728061c1f616
DVMaja <majadreilinger@gmail.com>
2023-10-17 14:48:17 +0200

    elérési útvonalak

--- potdoga/routes/web.php
+++ potdoga/routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/clubs', [ClubController::class, 'index']);
+Route::get('/api/clubs/{id}', [ClubController::class, 'show']);
+
+Route::post('/api/clubs', [ClubController::class, 'store']);
+
+Route::get('/api/users', [UserController::class,'index']);
+
+//ciew útvonalak következnek
+Route::get('/task/list', [ClubController::class, 'listView']);
+
+Route::get('/task/new', [ClubController::class, 'newView']);
+Route::get('/task/edit/{id}', [ClubController::class, 'editView']);
+
+Route::get('/api/users/', [UserController::class, 'index']);
+Route::get('/api/users/{{$user->id}}', [UserController::class, 'show']);
+Route::get('/user/list', [UserController::class, 'listView']);
