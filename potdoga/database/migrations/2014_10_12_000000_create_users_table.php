8d8572fbd215fabc234021b27a1221871c0313e7
DVMaja <majadreilinger@gmail.com>
2023-10-17 14:14:22 +0200

    adatbázis kapcsolattok megvannak

--- potdoga/database/migrations/2014_10_12_000000_create_users_table.php
+++ potdoga/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,11 +16,25 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();            
             $table->rememberToken();
             $table->timestamps();
         });
+
+        User::create([
+            'name' => "Nagy Péter", 
+            'email' => 'peti@gmail.com',                        
+        ]);
+
+        User::create([
+            'name' => "Kiss Bogi", 
+            'email' => 'bogi@gmail.com',                        
+        ]);
+
+        User::create([
+            'name' => "Szegedi Linda", 
+            'email' => 'linda@gmail.com',                        
+        ]);
     }
 
     /**
